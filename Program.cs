01a84e509177961c044a51ac801bd96f593eb5bd
CHEERASAK SAYNAK <cheerasak.s@psu.ac.th>
2025-04-10 15:47:35 +0700

    enable swagger page on prod

--- Program.cs
+++ Program.cs
@@ -20,11 +20,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
